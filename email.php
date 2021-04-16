37a1e80287127fc817f85c3615c8cf3abec133a2
MonsieurCris <82479048+MonsieurCris@users.noreply.github.com>
2021-04-15 22:24:44 -0300

    Add files via upload
    Teste php

---
+++ email.php
@@ -0,0 +1,32 @@
+<?php
+
+
+if(isset($_POST (['email'] && !empty($_POST('email')))))
+
+$nome = addslashes($_POST['nome'])
+$email = addslashes($_POST['email'])
+$mensagem = addslashes($_POST['mensagem'])
+
+
+$to = "cristhoperoliveira@gmail.com";
+$subject = "Contato - FrontEnd";
+$body = "Nome: " .$nome. "\n"
+        "Email: " .$email. "\n"
+        "Nome: " .$mensagem;
+
+$header = "From: cristhoper.victor@gmail.com" "\r\n"
+            ."Reply-To:".$email."\e\n"
+            ."X=Mailer:PHP/".phpversion();
+
+if(mail($to,$subject,$body,$header)){
+    
+    echo("Email Enviado Com Sucesso")
+
+}else{
+    echo("O Email não pode ser enviado")";"
+}
+
+
+
+
+}
